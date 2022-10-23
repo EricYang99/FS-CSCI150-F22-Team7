68066495b3e84938d3848a7060a78965e10ba5d5
Jonathan18380 <104102840+Jonathan18380@users.noreply.github.com>
2022-10-22 17:17:13 -0700

    Professor's page
    Page template of professors

---
+++ JohnTesting/testing.js
@@ -0,0 +1,491 @@
+
+let csciNamesAndPages = [
+	{
+		"Names": "CSCI 1. Critical Thinking and Computer Science",
+		"pageName": "csciOnePage.html"
+	},
+	{
+		"Names": "CSCI 5. Computer and Applications",
+		"pageName": "csciFivePage.html"
+	},
+	{
+		"Names": "CSCI 30. Introduction to the Internet",
+		"pageName": "csciThirdyPage.html"
+	},
+	{
+		"Names": "CSCI 39. Computer Science Principles",
+		"pageName": "csciThirdyNinePage.html"
+	},
+	{
+		"Names": "CSCI 40. Introduction to Programming and Problem Solving",
+		"pageName": "csciFourtyPage.html"
+	},
+	{
+		"Names": "CSCI 41. Introduction to Data Structures",
+		"pageName": "csciFourtyOnePage.html"
+	},
+	{
+		"Names": "CSCI 60. Foundations of Computer Science",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 68. Computing Practice and Programming",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 100. Introduction to Computational Science",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 101. Computational Foundations for Bioinformatics",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 108. Algorithmic Thinking",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 112. Introduction to Computer Systems",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 113. Introduction to Computer Organization",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 114. Introduction to Operating Systems",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 115. Algorithms and Data Structures",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 117. Structures of Programming Languages",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 119. Introduction to Finite Automata",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 124. Introduction to File Processing",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 126. Database Systems",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 130. Web Programming",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 134. Compiler Design",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 146. Systems Architecture",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 148. Systems Programming",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 150. Introduction to Software Engineering",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 152E. Software Engineering",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 154. Simulation",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 156. Internetworking Systems and Protocols",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 164. Artificial Intelligence Programming",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 166. Principles of Artificial Intelligence",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 168E. Big Data Analytics",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 172. Computer Graphics",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 173. Advanced Computer Graphics",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 174. Design and Analysis of Algorithms",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 176. Parallel Processing",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 177. Distributed Computer Systems",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 186. Formal Languages and Automata",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 188. Introduction to Computability",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 190. Independent Study",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 191T. Topics in Computer Science",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 191T. Computer Security",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 194I. Cooperative Education",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 198. Project",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 200. Introduction to Research in Computer Science",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 201. CSCI Colloquium",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 213. Computer Organization",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 217. Programming Language Principles",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 226. Advanced Database Systems",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 230. Advanced Web Application Development",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 244. Operating Systems",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 246. Computer Architecture",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 250. Advanced Software Engineering",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 252. Software Development and Environments",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 253. Human-Computer Interaction",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 256. Wireless Communications and Mobile Computing",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 264. Artificial Intelligence",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 272. Computer Graphics",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 274. Combinatorial Algorithms",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 282. Theory of Computation",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 284. Automata Theory",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 290. Independent Study",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 291T. Seminar",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 291T. Introduction to Reinforcement Learning",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 291T. Image Processing",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 297. Grad Synthesis",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 298. Research Project",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 298C. Project Continuation",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 299. Master's Thesis",
+		"pageName": ""
+	},
+	{
+		"Names": "CSCI 299C. Thesis Continuation",
+		"pageName": ""
+	}
+]
+let csciNames = [];
+for(let i = 0; i < csciNamesAndPages.length; i++){
+    csciNames.push(csciNamesAndPages[i]["Names"]);
+}
+
+//This is the auto complete search bar functions. It takes an array and will help auto fill the info based on input
+function autocomplete(inp, arr) {
+	/*the autocomplete function takes two arguments,
+	the text field element and an array of possible autocompleted values:*/
+	var currentFocus;
+	/*execute a function when someone writes in the text field:*/
+	inp.addEventListener("input", function(e) {
+		var a, b, i, val = this.value;
+		/*close any already open lists of autocompleted values*/
+		closeAllLists();
+		if (!val) { return false;}
+		currentFocus = -1;
+		/*create a DIV element that will contain the items (values):*/
+		a = document.createElement("DIV");
+		a.setAttribute("id", this.id + "autocomplete-list");
+		a.setAttribute("class", "autocomplete-items");
+		/*append the DIV element as a child of the autocomplete container:*/
+		this.parentNode.appendChild(a);
+		/*for each item in the array...*/
+		for (i = 0; i < arr.length; i++) {
+			/*check if the item starts with the same letters as the text field value:*/
+			if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+				/*create a DIV element for each matching element:*/
+				b = document.createElement("DIV");
+				/*make the matching letters bold:*/
+				b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+				b.innerHTML += arr[i].substr(val.length);
+				/*insert a input field that will hold the current array item's value:*/
+				b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+				/*execute a function when someone clicks on the item value (DIV element):*/
+					b.addEventListener("click", function(e) {
+					/*insert the value for the autocomplete text field:*/
+					inp.value = this.getElementsByTagName("input")[0].value;
+					/*close the list of autocompleted values,
+					(or any other open lists of autocompleted values:*/
+					closeAllLists();
+				});
+				a.appendChild(b);
+			}
+		}
+	});
+
+	/*execute a function presses a key on the keyboard:*/
+	inp.addEventListener("keydown", function(e) {
+		var x = document.getElementById(this.id + "autocomplete-list");
+		if (x) x = x.getElementsByTagName("div");
+		if (e.keyCode == 40) {
+			/*If the arrow DOWN key is pressed,
+			increase the currentFocus variable:*/
+			currentFocus++;
+			/*and and make the current item more visible:*/
+			addActive(x);
+			} else if (e.keyCode == 38) { //up
+			/*If the arrow UP key is pressed,
+			decrease the currentFocus variable:*/
+			currentFocus--;
+			/*and and make the current item more visible:*/
+			addActive(x);
+			} else if (e.keyCode == 13) {
+			/*If the ENTER key is pressed, prevent the form from being submitted,*/
+			e.preventDefault();
+			if (currentFocus > -1) {
+				/*and simulate a click on the "active" item:*/
+				if (x) x[currentFocus].click();
+			}
+		}
+	});
+
+	function addActive(x) {
+		/*a function to classify an item as "active":*/
+		if (!x) return false;
+		/*start by removing the "active" class on all items:*/
+		removeActive(x);
+		if (currentFocus >= x.length) currentFocus = 0;
+		if (currentFocus < 0) currentFocus = (x.length - 1);
+		/*add class "autocomplete-active":*/
+		x[currentFocus].classList.add("autocomplete-active");
+	}
+	function removeActive(x) {
+		/*a function to remove the "active" class from all autocomplete items:*/
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.remove("autocomplete-active");
+		}
+	}
+	function closeAllLists(elmnt) {
+		/*close all autocomplete lists in the document,
+		except the one passed as an argument:*/
+		var x = document.getElementsByClassName("autocomplete-items");
+		for (var i = 0; i < x.length; i++) {
+			if (elmnt != x[i] && elmnt != inp) {
+				x[i].parentNode.removeChild(x[i]);
+			}
+		}
+	}
+  	/*execute a function when someone clicks somewhere else in the document:*/
+  	document.addEventListener("click", function (e) {
+		closeAllLists(e.target);
+  	});
+}
+
+function aboutUs(){
+	deleteAll();
+	let bod = document.getElementById("container");
+	let newDiv = document.createElement('DIV');
+	newDiv.style.alignContent = "center";
+	newDiv.style.backgroundColor = "green";
+	let h1 = document.createElement('h1');
+	h1.style.textAlign = "center";
+	h1.appendChild(document.createTextNode("WE ARE CODING TURTLE ORGANISMS!"));
+	newDiv.appendChild(h1);
+	bod.appendChild(newDiv);
+}
+
+function departments(){
+	deleteAll();
+	let bod = document.getElementById("container");
+	let newDiv = document.createElement('DIV');
+	newDiv.style.alignContent = "center";
+	newDiv.style.backgroundColor = "blue";
+	let h1 = document.createElement('h1');
+	h1.style.textAlign = "center";
+	h1.appendChild(document.createTextNode("WE ARE CODING SHEEP ORGANISMS!"));
+	newDiv.appendChild(h1);
+	bod.appendChild(newDiv);
+}
+
+function faculty(){
+	deleteAll();
+	let bod = document.getElementById("container");
+	let newDiv = document.createElement('DIV');
+	newDiv.style.alignContent = "center";
+	newDiv.style.backgroundColor = "red";
+	let h1 = document.createElement('h1');
+	h1.style.textAlign = "center";
+	h1.appendChild(document.createTextNode("WE ARE CODING DOG ORGANISMS!"));
+	newDiv.appendChild(h1);
+	bod.appendChild(newDiv);
+}
+
+function loadClassTemplate(){
+	deleteAll()
+	let bod = document.getElementById("container");
+
+	//Start of flex-container
+	let flexContainer = document.createElement('div');
+		flexContainer.className = "flex-container";
+
+		//Start of tab
+		let tab = document.createElement('div');
+			tab.className = "tab";
+			tab.style.backgroundColor = "#13284c";
+			let tablink1 = document.createElement('button');
+				tablink1.className = "tablinks";
+				tablink1.onclick = "";
+				tablink1.appendChild(document.createTextNode("Class Description"));
+
+			let tablink2 = document.createElement('button');
+				tablink2.className = "tablinks";
+				tablink2.onclick = "";
+				tablink2.appendChild(document.createTextNode("Syllabus"));
+
+			let tablink3 = document.createElement('button');
+				tablink3.className = "tablinks";
+				tablink3.onclick = "";
+				tablink3.appendChild(document.createTextNode("Additional Info"));
+
+			tab.appendChild(tablink1);
+			tab.appendChild(tablink2);
+			tab.appendChild(tablink3);
+		//End of tab
+
+		 //Start of classInfo
+		let classInfo = document.createElement('div');
+			classInfo.id = "classInfo";
+			let flexContainerRow = document.createElement('div'); //Start of flexContainerRow
+				flexContainerRow.className = "flex-containerRow";
+				let classTitle = document.createElement('div');
+					let temp1H1 = document.createElement('h1');
+						temp1H1.appendChild(document.createTextNode("CSCI XXX"))
+					classTitle.className = "classTitle";
+					classTitle.appendChild(temp1H1);
+				let classTitle2 = document.createElement('div');
+					let temp2H1 = document.createElement('h1');
+						temp2H1.appendChild(document.createTextNode("Software Engineering"))
+					classTitle2.className = "classTitle2";
+					classTitle2.appendChild(temp2H1);
+				flexContainerRow.appendChild(classTitle);
+				flexContainerRow.appendChild(classTitle2);
+			classInfo.appendChild(flexContainerRow);
+		//End of classInfo
+		let instructorImg = document.createElement('img');
+			instructorImg.id = "instructor";
+			instructorImg.src = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png";
+			instructorImg.style.width = "150px";
+			instructorImg.style.height = "150px";
+		let classDescript = document.createElement('p');
+			classDescript.id = "classDescript"
+			classDescript.appendChild(document.createTextNode("Captians log, As i work on this project, I learn at how little I know. As i constantly tinker and praddle at the keys on my keyboard, I begin to understand the journey i have undertaken."));
+
+		flexContainer.appendChild(tab);
+		flexContainer.appendChild(classInfo);
+		flexContainer.appendChild(instructorImg);
+		flexContainer.appendChild(classDescript);
+	//End of flex-container
+	bod.appendChild(flexContainer);
+}
+
+function deleteAll(){
+	let bod = document.getElementById("container");
+	bod.innerHTML = '';
+}
